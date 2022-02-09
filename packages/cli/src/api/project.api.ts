aa290a802a5d6894bd9c9218151fedf1810399a8
Johann-Michael Thiebaut <johann.thiebaut@gmail.com>
2022-02-09 15:54:31 +0100

    Add command to upload a new project build to Meticulous

--- packages/cli/src/api/project.api.ts
+++ packages/cli/src/api/project.api.ts
@@ -1,17 +1,15 @@
-import axios, { AxiosInstance, AxiosError } from "axios";
+import axios, { AxiosInstance } from "axios";
 
 export const getProject: (client: AxiosInstance) => Promise<any> = async (
   client
 ) => {
-  const { data: projectData } = await client
-    .get("projects/token-info")
-    .catch((error) => {
-      if (axios.isAxiosError(error)) {
-        if (error.response?.status === 404) {
-          return { data: null };
-        }
+  const { data } = await client.get("projects/token-info").catch((error) => {
+    if (axios.isAxiosError(error)) {
+      if (error.response?.status === 404) {
+        return { data: null };
       }
-      throw error;
-    });
-  return projectData;
+    }
+    throw error;
+  });
+  return data;
 };
