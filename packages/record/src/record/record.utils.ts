8d00426a966e1b45669ac71605bf86b94b196f52
Quentin <q@meticulous.ai>
2022-09-23 11:50:10 +0100

    Enable rrweb recording only in CLI

--- packages/record/src/record/record.utils.ts
+++ packages/record/src/record/record.utils.ts
@@ -54,6 +54,7 @@ export async function bootstrapPage({
         window["METICULOUS_APP_COMMIT_HASH"] = "${appCommitHash}";
         window["METICULOUS_FORCE_RECORDING"] = true;
         window["METICULOUS_UPLOAD_INTERVAL_MS"] = ${uploadIntervalMs};
+        window["METICULOUS_ENABLE_RRWEB"] = true;
         window["METICULOUS_ENABLE_RRWEB_PLUGIN_NODE_DATA"] = true;
       `);
         await frame.evaluate(recordingSnippetFile);
