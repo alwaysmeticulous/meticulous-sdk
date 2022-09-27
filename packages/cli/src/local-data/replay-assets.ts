14c14f45010c1fe4aecf655bdac4b6008b89577f
Alex Ivanov <alex@meticulous.ai>
2022-09-27 17:43:53 +0100

    rename function

--- packages/cli/src/local-data/replay-assets.ts
+++ packages/cli/src/local-data/replay-assets.ts
@@ -21,14 +21,17 @@ export interface AssetMetadata {
 const ASSETS_FOLDER_NAME = "assets";
 const ASSET_METADATA_FILE_NAME = "assets.json";
 
-export const getCreateAssetsDir: () => Promise<string> = async () => {
+export const getOrCreateAssetsDir: () => Promise<string> = async () => {
   const assetsDir = join(getMeticulousLocalDataDir(), ASSETS_FOLDER_NAME);
   await mkdir(assetsDir, { recursive: true });
   return assetsDir;
 };
 
 export const loadAssetMetadata: () => Promise<AssetMetadata> = async () => {
-  const assetsFile = join(await getCreateAssetsDir(), ASSET_METADATA_FILE_NAME);
+  const assetsFile = join(
+    await getOrCreateAssetsDir(),
+    ASSET_METADATA_FILE_NAME
+  );
 
   const existingMetadata = await readFile(assetsFile)
     .then((data) => JSON.parse(data.toString("utf-8")))
@@ -43,7 +46,10 @@ export const loadAssetMetadata: () => Promise<AssetMetadata> = async () => {
 export const saveAssetMetadata: (
   assetMetadata: AssetMetadata
 ) => Promise<void> = async (assetMetadata) => {
-  const assetsFile = join(await getCreateAssetsDir(), ASSET_METADATA_FILE_NAME);
+  const assetsFile = join(
+    await getOrCreateAssetsDir(),
+    ASSET_METADATA_FILE_NAME
+  );
 
   await writeFile(assetsFile, JSON.stringify(assetMetadata, null, 2));
 };
@@ -60,7 +66,7 @@ export const fetchAsset: (path: string) => Promise<string> = async (path) => {
     (item) => item.fileName === assetFileName
   );
   const fileName = assetFileName;
-  const filePath = join(await getCreateAssetsDir(), fileName);
+  const filePath = join(await getOrCreateAssetsDir(), fileName);
 
   if (entry && etag && etag === entry.etag) {
     logger.debug(`${fetchUrl} already present`);
