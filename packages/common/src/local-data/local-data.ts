1e89a9bb519e3dd0937281fc8b99c8f3ecbd553c
Gabriel Spencer-Harper <gabriel.s.harper@gmail.com>
2022-10-20 16:19:36 +0100

    Make set function side effect more explicit by returning void

--- packages/common/src/local-data/local-data.ts
+++ packages/common/src/local-data/local-data.ts
@@ -12,18 +12,16 @@ export const getMeticulousLocalDataDir: () => string = () => {
 
 export const setMeticulousLocalDataDir: (
   localDataDir?: string | null | undefined
-) => string = (localDataDir) => {
+) => void = (localDataDir) => {
   const logger = log.getLogger(METICULOUS_LOGGER_NAME);
   if (_localDataDir) {
     logger.warn(
       `Meticulous local data dir has already been set by a prior call of setMeticulousLocalDataDir `
     );
-    return _localDataDir;
   }
 
   _localDataDir =
     localDataDir ||
     process.env["METICULOUS_DIR"] ||
     normalize(join(process.env["HOME"] || process.cwd(), ".meticulous"));
-  return _localDataDir;
 };
