f7ff4fe519fdeedb648487151e25f6f3c8465032
Johann-Michael Thiebaut <johann.thiebaut@gmail.com>
2022-10-26 16:05:51 +0200

    Add eslint plugin for imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,20 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
   ],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
   },
 };
