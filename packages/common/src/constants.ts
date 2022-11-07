847e487b92892a372b17fbe9b2cfe99616be50f8
Gabriel <1615812+Gabriel-H@users.noreply.github.com>
2022-11-07 15:07:35 +0000

    add comments (#150)

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -1,19 +1,43 @@
 export const BASE_SNIPPETS_URL = "https://snippet.meticulous.ai/";
 
+// Many comments directly from Chromium source
+// https://source.chromium.org/chromium/chromium/src/+/main:content/public/common/content_switches.cc
 export const COMMON_CHROMIUM_FLAGS = [
-    "--disable-features=Translate",
-    "--disable-background-timer-throttling",
-    "--disable-renderer-backgrounding",
-    "--disable-backgrounding-occluded-windows",
-    "--disable-breakpad",
-    "--disable-client-side-phishing-detection",
-    "--disable-default-apps",
-    "--disable-dev-shm-usage",
-    "--disable-extensions",
-    "--disable-hang-monitor",
-    "--disable-ipc-flooding-protection",
-    "--disable-notifications",
-    "--disable-prompt-on-repost",
-    "--disable-sync",
-    "--no-first-run",
+  "--disable-features=Translate",
+  // Disable task throttling of timer tasks from background pages.
+  "--disable-background-timer-throttling",
+  // Prevent renderer process backgrounding when set.
+  "--disable-renderer-backgrounding",
+  // Disable backgrounding renders for occluded windows. Done for tests to avoid
+  // nondeterministic behavior.
+  "--disable-backgrounding-occluded-windows",
+  // Disables crash reporting. ↪
+  "--disable-breakpad",
+  "--disable-client-side-phishing-detection",
+  // Disables installation of default apps on first run, e.g. gmail
+  "--disable-default-apps",
+  // The /dev/shm partition is too small in certain VM environments, causing
+  // Chrome to fail or crash (see http://crbug.com/715363). Use this flag to
+  // work-around this issue (a temporary directory will always be used to create
+  // anonymous shared memory files).
+  "--disable-dev-shm-usage",
+  "--disable-extensions",
+  // Suppresses hang monitor dialogs in renderer processes.
+  // This may allow slow unload handlers on a page to prevent the tab from closing,
+  // but the Task Manager can be used to terminate the offending process in this case.
+  "--disable-hang-monitor",
+  // Disables the IPC flooding protection.
+  // It is activated by default. Some javascript functions can be used to flood
+  // the browser process with IPC. This protection limits the rate at which they
+  // can be used.
+  "--disable-ipc-flooding-protection",
+  // Disables the Web Notification and the Push APIs.
+  "--disable-notifications",
+  // Normally when the user attempts to navigate to a page that was the result of a post
+  // we prompt to make sure they want to. This switch may be used to disable that check.
+  "--disable-prompt-on-repost",
+  "--disable-sync",
+  // Skip First Run tasks, like importing history or bookmarks.
+  // See https://www.chromium.org/developers/design-documents/first-run-customizations/
+  "--no-first-run",
 ];
