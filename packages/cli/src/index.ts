66e83023a546544975893fb658082d375114b410
Quentin <q@meticulous.ai>
2022-11-23 16:25:25 +0000

    Export more types

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -9,4 +9,8 @@ export { runAllTestsCommand } from "./commands/run-all-tests/run-all-tests.comma
 export { screenshotDiffCommand } from "./commands/screenshot-diff/screenshot-diff.command";
 export { showProjectCommand } from "./commands/show-project/show-project.command";
 export { updateTestsCommand } from "./commands/update-tests/update-tests.command";
-export { runAllTests } from "./parallel-tests/run-all-tests";
+export {
+  runAllTests,
+  TestRun,
+  RunAllTestsResult,
+} from "./parallel-tests/run-all-tests";
