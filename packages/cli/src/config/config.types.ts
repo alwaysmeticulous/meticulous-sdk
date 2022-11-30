949f3a6b140c03b9ee0d3e2516086b1bdb82429e
Quentin <q@meticulous.ai>
2022-11-30 11:01:28 +0000

    jsdoc

--- packages/cli/src/config/config.types.ts
+++ packages/cli/src/config/config.types.ts
@@ -6,6 +6,12 @@ export interface MeticulousCliConfig {
 
 export interface TestCaseResult extends TestCase {
   headReplayId: string;
+
+  /**
+   * A test case is marked as a flake if there were screenshot comparison failures,
+   * but for every one of those failures regenerating the screenshot on head sometimes gave
+   * a different screenshot to the original screenshot taken on head.
+   */
   result: "pass" | "fail" | "flake";
 }
 
