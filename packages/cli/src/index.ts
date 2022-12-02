d8f2f5fad35c4a6ab6bacb660eaf2fb58bccbb90
Johann-Michael Thiebaut <johann.thiebaut@gmail.com>
2022-12-02 11:21:51 +0100

    export

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,14 +1,18 @@
+export {
+  getLatestTestRunResults,
+  GetLatestTestRunResultsOptions,
+} from "./api/test-run.api";
 export { bootstrapCommand } from "./commands/bootstrap/bootstrap.command";
 export { createTestCommand } from "./commands/create-test/create-test.command";
 export { debugReplayCommand } from "./commands/debug-replay/debug-replay.command";
 export { downloadReplayCommand } from "./commands/download-replay/download-replay.command";
 export { downloadSessionCommand } from "./commands/download-session/download-session.command";
-export { showTestRunCommand } from "./commands/show-test-run/show-test-run.command";
 export { recordCommand } from "./commands/record/record.command";
 export { replayCommand } from "./commands/replay/replay.command";
 export { runAllTestsCommand } from "./commands/run-all-tests/run-all-tests.command";
 export { screenshotDiffCommand } from "./commands/screenshot-diff/screenshot-diff.command";
 export { showProjectCommand } from "./commands/show-project/show-project.command";
+export { showTestRunCommand } from "./commands/show-test-run/show-test-run.command";
 export { updateTestsCommand } from "./commands/update-tests/update-tests.command";
 export {
   runAllTests,
