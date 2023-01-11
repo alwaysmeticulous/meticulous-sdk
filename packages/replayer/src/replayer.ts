288e3a7ec68f678d2121a03044a40bf52be36599
Alex Ivanov <alex@meticulous.ai>
2023-01-11 11:09:18 +0200

    handle simulation timeout errors (#217)
    This terminates the replay early if timeout occurs while replaying user
    interaction events

--- packages/replayer/src/replayer.ts
+++ packages/replayer/src/replayer.ts
@@ -293,7 +293,11 @@ export const replayEvents: ReplayEventsFn = async (options) => {
       }
     }
   } catch (error) {
-    logger.error("Error thrown during replay", error);
+    if (error instanceof Error && error.name === "MeticulousTimeoutError") {
+      await browser.close();
+      throw error;
+    }
+
     onTimelineEvent({
       kind: "fatalError",
       start: new Date().getTime(),
