7a398d820451b02721afae6cb4f3955889cbfb8c
Quentin <q@meticulous.ai>
2023-03-21 15:22:44 +0000

    Comments

--- packages/cli/src/commands/screenshot-diff/utils/get-screenshot-filename.ts
+++ packages/cli/src/commands/screenshot-diff/utils/get-screenshot-filename.ts
@@ -1,5 +1,7 @@
 import { ScreenshotIdentifier } from "@alwaysmeticulous/api";
 
+// Note: ideally this should match the filenames produced by the screenshotting code
+// in replay-node, and in https://github.com/alwaysmeticulous/meticulous-sdk/blob/395af4394dc51d9b51ba1136fc26b23fcbba5604/packages/replayer/src/screenshot.utils.ts#L42
 export const getScreenshotFilename = (identifier: ScreenshotIdentifier) => {
   if (identifier.type === "end-state") {
     return "final-state.png";
