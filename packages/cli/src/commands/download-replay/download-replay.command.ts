5052d1dbc9e26698b6d349b963ef80fff9bbafc8
Quentin <q@meticulous.ai>
2023-06-13 16:48:17 +0100

    Auto-generate logs.concise.txt

--- packages/cli/src/commands/download-replay/download-replay.command.ts
+++ packages/cli/src/commands/download-replay/download-replay.command.ts
@@ -1,9 +1,13 @@
+import { readFileSync, writeFileSync } from "fs";
+import { access, readFile, writeFile } from "fs/promises";
+import { join } from "path";
 import { createClient } from "@alwaysmeticulous/client";
 import { METICULOUS_LOGGER_NAME } from "@alwaysmeticulous/common";
 import {
   getOrFetchReplay,
   getOrFetchReplayArchive,
 } from "@alwaysmeticulous/downloading-helpers";
+import { fileExists } from "@alwaysmeticulous/downloading-helpers/dist/file-downloads/local-data.utils";
 import log from "loglevel";
 import { buildCommand } from "../../command-utils/command-builder";
 
@@ -28,6 +32,31 @@ const handler: (options: Options) => Promise<void> = async ({
     client,
     replayId
   );
+
+  // Generate logs.concise.txt file
+  const logsFile = join(replayFolderFilePath, "logs.json");
+  const logsFileExists = await access(logsFile)
+    .then(() => true)
+    .catch(() => false);
+  if (logsFileExists) {
+    try {
+      const logs = JSON.parse(
+        await readFile(join(replayFolderFilePath, "logs.json"), "utf8")
+      );
+      const conciseLogs = logs.console.map(
+        (log: { type: string; message: string }) => {
+          return log.message.replace("[METICULOUS] ", "");
+        }
+      );
+      await writeFile(
+        join(replayFolderFilePath, "logs.concise.txt"),
+        conciseLogs.join("\n")
+      );
+    } catch (err) {
+      logger.error("Error creating concise version of logs file", err);
+    }
+  }
+
   logger.info(`Downloaded replay data to: ${replayFolderFilePath}`);
 };
 
