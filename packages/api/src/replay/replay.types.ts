3bebf7387b7269138361487085d62c1be9a144c6
Alex Ivanov <alex@meticulous.ai>
2023-10-06 14:32:31 +0100

    Add support for downloading V3 replays

--- packages/api/src/replay/replay.types.ts
+++ packages/api/src/replay/replay.types.ts
@@ -3,4 +3,5 @@ import { Project } from "../project.types";
 export interface Replay {
   id: string;
   project: Project;
+  version: "v1" | "v2" | "v3";
 }
--- packages/client/src/api/replay.api.ts
+++ packages/client/src/api/replay.api.ts
@@ -37,3 +37,32 @@ export const getReplayDownloadUrl: (
     });
   return data;
 };
+
+export interface S3UploadLocation {
+  filePath: string; // e.g. logs.ndjson.gz
+  signedUrl: string; // signed S3 URL
+}
+
+export type ReplayV3UploadLocations = Record<string, S3UploadLocation> & {
+  screenshots: Record<
+    string,
+    { image: S3UploadLocation; metadata: S3UploadLocation }
+  >;
+};
+
+export const getReplayV3DownloadUrls: (
+  client: AxiosInstance,
+  replayId: string
+) => Promise<ReplayV3UploadLocations | null> = async (client, replayId) => {
+  const { data } = await client
+    .get<ReplayV3UploadLocations>(`replays/${replayId}/download-urls`)
+    .catch((error) => {
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 404) {
+          return { data: null };
+        }
+      }
+      throw error;
+    });
+  return data;
+};
