a44c0a7073f0b60822d94a51ece7d8a096fc6e93
Quentin <q@meticulous.ai>
2023-12-06 19:10:15 +0000

    Fix bug in capturing cookies too early

--- packages/record/src/record/utils/provide-cookie-access.ts
+++ packages/record/src/record/utils/provide-cookie-access.ts
@@ -5,26 +5,29 @@ type ModifiedWindow = {
   __meticulous?: {
     getAllCookies?: () => Promise<Cookie[]>;
   };
+  __meticulous_getAllCookies?: () => Promise<Cookie[]>;
 };
 
 export const provideCookieAccess = async (page: Page) => {
   const client = await page.target().createCDPSession();
-  const cookies = (await client.send("Network.getAllCookies")).cookies.map(
-    ({ sameSite, ...rest }): Cookie => {
-      const convertedSameSite = convertSameSiteValue(sameSite);
-      return {
-        ...rest,
-        ...(convertedSameSite ? { sameSite: convertedSameSite } : {}),
-      };
-    }
-  );
-  page.evaluateOnNewDocument((cookies) => {
+  const getCookies = async () =>
+    (await client.send("Network.getAllCookies")).cookies.map(
+      ({ sameSite, ...rest }): Cookie => {
+        const convertedSameSite = convertSameSiteValue(sameSite);
+        return {
+          ...rest,
+          ...(convertedSameSite ? { sameSite: convertedSameSite } : {}),
+        };
+      }
+    );
+  await page.exposeFunction("__meticulous_getAllCookies", getCookies);
+  page.evaluateOnNewDocument(() => {
     const meticulousObject = (window as ModifiedWindow).__meticulous ?? {};
-    meticulousObject.getAllCookies = async () => {
-      return cookies;
-    };
+    meticulousObject.getAllCookies = (
+      window as ModifiedWindow
+    ).__meticulous_getAllCookies!;
     (window as ModifiedWindow).__meticulous = meticulousObject;
-  }, cookies);
+  });
 };
 
 const convertSameSiteValue = (
