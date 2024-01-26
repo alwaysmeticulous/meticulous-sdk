ef6986d9fb6fbed0441dbd3086ed5aeeded7a6aa
Alex Ivanov <alex@meticulous.ai>
2024-01-26 15:41:25 +0000

    bypass CSP for record-login by default

--- packages/cli/src/commands/record-login-flow/record-login-flow.command.ts
+++ packages/cli/src/commands/record-login-flow/record-login-flow.command.ts
@@ -85,5 +85,13 @@ export const recordLoginFlowCommand = buildCommand("record-login")
   .details({
     describe: "Record a login flow session",
   })
-  .options(COMMON_RECORD_OPTIONS)
+  .options({
+    ...COMMON_RECORD_OPTIONS,
+
+    // We explicitly set the default to true for this command.
+    bypassCSP: {
+      ...COMMON_RECORD_OPTIONS.bypassCSP,
+      default: true,
+    },
+  })
   .handler(recordLoginFlowCommandHandler);
