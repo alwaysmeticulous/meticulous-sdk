65f624b6b43b452ad36f112a3ba016b0a190d339
Quentin <q@meticulous.ai>
2024-02-08 16:41:45 +0000

    Tone down data dir warnings: the local data dir defaults to ~/.meticulous and this is normally what is desired and relyed upon

--- packages/common/src/local-data/local-data.ts
+++ packages/common/src/local-data/local-data.ts
@@ -7,10 +7,10 @@ let _localDataDir = "";
 export const getMeticulousLocalDataDir: () => string = () => {
   const logger = log.getLogger(METICULOUS_LOGGER_NAME);
   if (!_localDataDir) {
-    logger.warn(
-      "Local data dir has not been set by setMeticulousLocalDataDir()!"
-    );
     setMeticulousLocalDataDir();
+    logger.debug(
+      `Local data dir has not been set explictly, so defaulting to ${_localDataDir}`
+    );
   }
   logger.debug(`Meticulous local data dir: ${_localDataDir}`);
   return _localDataDir;
