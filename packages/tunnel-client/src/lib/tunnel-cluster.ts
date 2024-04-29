a18cd1582daffdd88051dd602bd459e3fd5e8ee3
Alex Ivanov <alex@meticulous.ai>
2024-04-29 14:40:35 +0100

    Revert "Force close remote secure tunnel connections (#614)"
    This reverts commit 72fdc59e5f5d6b9ec29e1337cc16dab1c5ae9168.

--- packages/tunnel-client/src/lib/tunnel-cluster.ts
+++ packages/tunnel-client/src/lib/tunnel-cluster.ts
@@ -90,7 +90,7 @@ export class TunnelCluster extends (EventEmitter as new () => TypedEmitter<Tunne
         );
       }
 
-      remote.destroy();
+      remote.end();
     });
 
     const connLocal = () => {
@@ -164,7 +164,7 @@ export class TunnelCluster extends (EventEmitter as new () => TypedEmitter<Tunne
         remote.removeListener("close", remoteClose);
 
         if (err.code !== "ECONNREFUSED" && err.code !== "ECONNRESET") {
-          return remote.destroy();
+          return remote.end();
         }
 
         // retrying connection to local server
@@ -191,7 +191,6 @@ export class TunnelCluster extends (EventEmitter as new () => TypedEmitter<Tunne
         // when local closes, also get a new remote
         local.once("close", (hadError) => {
           this.logger.debug("local connection closed [%s]", hadError);
-          remote.destroy();
         });
       });
     };
