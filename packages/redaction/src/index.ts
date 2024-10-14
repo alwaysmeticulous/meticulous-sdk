cc4154401f241da73769be49a870fe8dbd09552b
Quentin <q@meticulous.ai>
2024-10-14 21:06:11 +0100

    Fix exports

--- packages/redaction/src/index.ts
+++ packages/redaction/src/index.ts
@@ -1,1 +1,4 @@
 export * from "./network-requests/drop-request-header";
+export * from "./network-responses/transform-json-response";
+export * from "./generic/redact-recursively";
+export * from "./generic/asterix-out";
