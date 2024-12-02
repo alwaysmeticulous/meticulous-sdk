f1d0c48d50b87c9da8b172de706d5ba171fe4827
Edoardo Pirovano <ed@meticulous.ai>
2024-12-02 09:06:39 +0000

    Fix typo

--- packages/api/src/sdk-bundle-api/sdk-to-bundle/session-data.ts
+++ packages/api/src/sdk-bundle-api/sdk-to-bundle/session-data.ts
@@ -31,7 +31,7 @@ export interface SessionData {
      * Only present on recordings since ~Aug 2024
      */
     indexedDb?: {
-      state: IDBObjectStoreWithEntries[];
+      state: IDBObjectStoreSnapshot[];
     };
   };
 
