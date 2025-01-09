64c1ec6eb23eeb001bca9e225988d3291ed3c3af
Edoardo Pirovano <ed@meticulous.ai>
2025-01-09 16:25:52 +0000

    Rename repo-url endpoint

--- packages/client/src/api/project.api.ts
+++ packages/client/src/api/project.api.ts
@@ -2,6 +2,14 @@ import { Project } from "@alwaysmeticulous/api";
 import { AxiosInstance, isAxiosError } from "axios";
 import { maybeEnrichAxiosError } from "../errors";
 
+export interface GetRepoUrlOptions {
+  client: AxiosInstance;
+}
+
+export interface RepoUrlResponse {
+  repoUrl: string;
+}
+
 export const getProject: (
   client: AxiosInstance
 ) => Promise<Project | null> = async (client) => {
@@ -14,5 +22,25 @@ export const getProject: (
 
       throw maybeEnrichAxiosError(error);
     });
+  return data;
+};
+
+export const getRepoUrl = async ({
+  client,
+}: GetRepoUrlOptions): Promise<RepoUrlResponse> => {
+  const { data } = await client
+    .get<unknown, { data: RepoUrlResponse }>("projects/repo-url")
+    .catch((error) => {
+      if (isAxiosError(error)) {
+        const errorMessage = error.response?.data?.message;
+
+        if (errorMessage) {
+          throw new Error(errorMessage);
+        }
+      }
+
+      throw error;
+    });
+
   return data;
 };
