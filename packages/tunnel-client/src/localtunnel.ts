cdb761f531d65dc6d1bc9e6a128e6bc724918e17
Edoardo Pirovano <6748066+edoardopirovano@users.noreply.github.com>
2025-01-29 20:23:09 +0000

    Fail early if opening a tunnel with the Bun runtime (#788)
    This won't work, so let's fail early rather than spending lots of time
    debugging a hanging connection like I just did.

--- packages/tunnel-client/src/localtunnel.ts
+++ packages/tunnel-client/src/localtunnel.ts
@@ -2,6 +2,12 @@ import { Tunnel } from "./lib/tunnel";
 import { LocalTunnelOptions } from "./types";
 
 export const localtunnel = (options: LocalTunnelOptions): Promise<Tunnel> => {
+  // Per https://bun.sh/guides/util/detect-bun this detects if the runtime is Bun
+  if (process.versions.bun) {
+    return new Promise((_, reject) => {
+      reject(new Error("Opening a tunnel is unsupported in the Bun runtime!"));
+    });
+  }
   const client = new Tunnel(options);
   return new Promise((resolve, reject) =>
     client.open((err) => (err ? reject(err) : resolve(client)))
