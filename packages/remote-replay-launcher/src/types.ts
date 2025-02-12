efe7b866e678765bfd9d9eda5228ee0d76b9592c
Edoardo Pirovano <6748066+edoardopirovano@users.noreply.github.com>
2025-02-12 13:42:19 +0000

    Update packages/remote-replay-launcher/src/types.ts
    Co-authored-by: Alex Ivanov <alex@meticulous.ai>

--- packages/remote-replay-launcher/src/types.ts
+++ packages/remote-replay-launcher/src/types.ts
@@ -34,7 +34,7 @@ export interface ExecuteRemoteTestRunOptions {
   /**
    * The ID of the pull request in the hosting provider, if one is available.
    * The backend will try to automatically find one from the commit if this is not provided, but in some
-   * cases (e.g. PRs from forks on GitHub) this may not work, hence we prefer to set this if possible.
+   * cases (e.g. PRs from forks on GitHub) this may not work, hence one can be provided here.
    */
   pullRequestHostingProviderId?: string;
 }
