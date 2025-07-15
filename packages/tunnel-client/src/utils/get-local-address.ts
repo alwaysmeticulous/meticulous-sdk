30bc7434459b164f07812bf2c180b6d609ee6f2e
Edoardo Pirovano <ed@meticulous.ai>
2025-07-15 14:43:29 +0100

    Address comments from Alex

--- packages/tunnel-client/src/utils/get-local-address.ts
+++ packages/tunnel-client/src/utils/get-local-address.ts
@@ -1,12 +1,10 @@
-const LOCAL_ADDRESS_ENV_VARIABLE = "METICULOUS_LOCAL_ADDRESS";
-const LOCAL_PORT_ENV_VARIABLE = "METICULOUS_LOCAL_PORT";
+const LOCAL_ADDRESS_ENV_VARIABLE =
+  "METICULOUS_TUNNEL_CLIENT_REQUEST_LOCAL_ADDRESS";
 
 export const getLocalAddress = () => {
   const localAddress = process.env[LOCAL_ADDRESS_ENV_VARIABLE];
-  const localPort = process.env[LOCAL_PORT_ENV_VARIABLE];
 
   return {
     ...(localAddress ? { localAddress } : {}),
-    ...(localPort ? { localPort: parseInt(localPort) } : {}),
   };
 };
