354cfc2c2d9497f3349bc4c7cb2ffd89efa1b6fe
Edoardo Pirovano <ed@meticulous.ai>
2025-10-13 10:30:01 +0100

    Tweak download-test-run command

--- packages/cli/src/commands/download-test-run/download-test-run.command.ts
+++ packages/cli/src/commands/download-test-run/download-test-run.command.ts
@@ -1,24 +1,44 @@
 import { createClient } from "@alwaysmeticulous/client";
-import { initLogger } from "@alwaysmeticulous/common";
-import { getOrFetchTestRunData } from "@alwaysmeticulous/downloading-helpers";
+import { initLogger, IS_METICULOUS_SUPER_USER } from "@alwaysmeticulous/common";
+import {
+  TEST_RUN_DOWNLOAD_SCOPES,
+  getOrFetchTestRunData,
+  TestRunDownloadScope,
+} from "@alwaysmeticulous/downloading-helpers";
 import { buildCommand } from "../../command-utils/command-builder";
 
 interface Options {
   apiToken?: string | null | undefined;
-  testRunId: string;
+  testRunId?: string | null | undefined;
+  scope?: string | null | undefined;
 }
+
+const ALLOWED_SCOPES: TestRunDownloadScope[] = ["coverage-only"];
 
 const handler: (options: Options) => Promise<void> = async ({
   apiToken,
   testRunId,
+  scope: _scope,
 }) => {
   const logger = initLogger();
   const client = createClient({ apiToken });
 
+  const scope = (_scope as TestRunDownloadScope) ?? "coverage-only";
+
+  if (!ALLOWED_SCOPES.includes(scope)) {
+    if (!IS_METICULOUS_SUPER_USER) {
+      logger.error(`Invalid scope: ${scope}`);
+      process.exit(1);
+    }
+    logger.warn(
+      `Downloading admin-only scope (files downloaded are subject to change with no warning)`,
+    );
+  }
+
   const { fileName: testRunDir } = await getOrFetchTestRunData(
     client,
-    testRunId,
-    "everything"
+    testRunId ?? "latest",
+    scope,
   );
   logger.info(`Downloaded test run data to: ${testRunDir}`);
 };
@@ -33,7 +53,14 @@ export const downloadTestRunCommand = buildCommand("download-test-run")
     },
     testRunId: {
       string: true,
-      demandOption: true,
+      default: "latest",
+    },
+    scope: {
+      string: true,
+      demandOption: false,
+      choices: [...TEST_RUN_DOWNLOAD_SCOPES],
+      default: "coverage-only",
+      hidden: !IS_METICULOUS_SUPER_USER,
     },
   })
   .handler(handler);
