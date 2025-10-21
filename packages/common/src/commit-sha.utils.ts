55bb0eb74ea56b2d5a01430e3e4055ddd7dc79d2
Francesco Parolini <parolini.francesco.1@gmail.com>
2025-10-21 15:54:00 +0100

    expose part of the new API for performance measurement

--- packages/common/src/commit-sha.utils.ts
+++ packages/common/src/commit-sha.utils.ts
@@ -37,3 +37,49 @@ export const getCommitSha: (
     return "";
   }
 };
+
+const getGitCommitDate: (commitSha: string) => Promise<string> = (
+  commitSha,
+) => {
+  return new Promise((resolve, reject) => {
+    exec(
+      `git show -s --format=%cI ${commitSha}`,
+      { encoding: "utf-8" },
+      (error, output) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(output);
+      },
+    );
+  });
+};
+
+export const getCommitDate: (
+  commitDate: string | null | undefined,
+  commitSha: string,
+) => Promise<string> = async (commitDate_, commitSha) => {
+  if (commitDate_) {
+    return commitDate_;
+  }
+
+  const logger = initLogger();
+
+  try {
+    const gitCommitDate = (await getGitCommitDate(commitSha)).trim();
+    return gitCommitDate;
+  } catch (error) {
+    // Suppress error logging if not in a git repository
+    if (error instanceof Error) {
+      if (error.message.startsWith("Command failed")) {
+        logger.info(
+          "Notice: not running in a git repository (cannot get commit date)",
+        );
+        return "";
+      }
+    }
+    logger.error(error);
+    return "";
+  }
+};
