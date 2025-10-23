c57f6d8184d5d1adbad91a242e29350a29ee0a3c
Edoardo Pirovano <6748066+edoardopirovano@users.noreply.github.com>
2025-10-23 11:24:06 +0100

    Fix duplicated type in one more place (#982)
    I don't know why this exists in three different places but let's make
    them all match for now.

--- packages/api/src/replay/test-run-chunk.types.ts
+++ packages/api/src/replay/test-run-chunk.types.ts
@@ -3,6 +3,7 @@ import { TestRunStatus } from "./test-run.types";
 /**
  * Execution of a chunk of a test run chunk.
  *
- * The values and their meanings are the same as for {@link TestRunStatus}.
+ * The values and their meanings are the same as for {@link TestRunStatus}, except
+ * it's not possible for a test run chunk to be in the `PostProcessing` status.
  */
-export type TestRunChunkStatus = TestRunStatus;
+export type TestRunChunkStatus = Omit<TestRunStatus, "PostProcessing">;
