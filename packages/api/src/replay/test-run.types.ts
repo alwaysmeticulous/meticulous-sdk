a82a14af8e68bbc0cf7204636c47390336db90cb
Edoardo Pirovano <6748066+edoardopirovano@users.noreply.github.com>
2025-10-23 10:10:48 +0100

    Omit PostProcessing from TestRunChunkStatus (#980)

--- packages/api/src/replay/test-run.types.ts
+++ packages/api/src/replay/test-run.types.ts
@@ -68,9 +68,10 @@ export type TestRunStatus =
 /**
  * Execution of a chunk of a test run chunk.
  *
- * The values and their meanings are the same as for {@link TestRunStatus}.
+ * The values and their meanings are the same as for {@link TestRunStatus}, except
+ * it's not possible for a test run chunk to be in the `PostProcessing` status.
  */
-export type TestRunChunkStatus = TestRunStatus;
+export type TestRunChunkStatus = Omit<TestRunStatus, "PostProcessing">;
 
 export type TestCaseResultStatus = "pass" | "fail" | "flake";
 
