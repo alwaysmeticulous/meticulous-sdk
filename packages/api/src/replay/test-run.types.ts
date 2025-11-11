03436948c4d5cc27cc2fc3160f281ae8f0e6ab0f
Quentin <q@meticulous.ai>
2025-11-11 10:29:34 +0000

    Update docs on types

--- packages/api/src/replay/test-run.types.ts
+++ packages/api/src/replay/test-run.types.ts
@@ -14,7 +14,7 @@ export enum SessionRelevance {
 }
 
 export const isPrAuthorRelevance = (
-  relevance: SessionRelevance | null | undefined
+  relevance: SessionRelevance | null | undefined,
 ): boolean => {
   if (!relevance) {
     return false;
@@ -48,9 +48,9 @@ export interface TestCaseReplayOptions extends Partial<ScreenshotDiffOptions> {
  *
  * `Running` = a worker is actively running the test run.
  *
- * `Failure` = completed, and at least one replay had notable differences - a diff, missing-head or different-size (see has-notable-differences.ts in the main repo)
+ * `Failure` = completed, and at least one replay had notable differences - a diff (see has-notable-differences.ts in the main repo)
  *
- * `Success` = completed, and no replays had notable differences
+ * `Success` = completed, and no replays had notable differences (i.e. just no differences, flakes, missing heads, missing bases or different sizes)
  *
  * `ExecutionError` = the test run failed fatally, and didn't complete. To get accurate results it'll need to be re-run. The test run may shortly switch back
  * into 'Running' in this case, if the worker retries it.
@@ -75,9 +75,11 @@ export interface TestCaseResult extends TestCase {
   headReplayId: string;
 
   /**
-   * A test case is marked as a flake if there were screenshot comparison failures,
-   * but for every one of those failures regenerating the screenshot on head sometimes gave
-   * a different screenshot to the original screenshot taken on head.
+   * A test case is marked as a failure if at least one screenshot had a reproducible (no flakey) difference.
+   *
+   * A test case is marked as a flake if there were differences but all differences were flakey/non-reproducible.
+   *
+   * Otherwise a test case is marked as a pass: all screenshots had no differences and no flakes.
    */
   result: TestCaseResultStatus;
 }
