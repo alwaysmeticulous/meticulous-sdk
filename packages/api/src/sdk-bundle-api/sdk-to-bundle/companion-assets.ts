36734622db5262c5dde63138ae1a093bc43cf993
Olaf Czarnecki <olaf@meticulous.ai>
2026-02-23 18:10:48 +0000

    Add deployment archive type to api

--- packages/api/src/sdk-bundle-api/sdk-to-bundle/companion-assets.ts
+++ packages/api/src/sdk-bundle-api/sdk-to-bundle/companion-assets.ts
@@ -1,3 +1,5 @@
+import { DeploymentArchiveType } from "./deployment-archive-type";
+
 /**
  * Information about companion assets to serve _together_ with a deployment. Note this
  * is distinct from a static assets deployment, which is _just_ static assets.
@@ -12,4 +14,9 @@ export interface CompanionAssetsInfo {
    * A regex to match to determine if a path should be served from the companion assets.
    */
   regex: string;
+
+  /**
+   * File format of the companion asset bundle
+   */
+  archiveType: DeploymentArchiveType;
 }
